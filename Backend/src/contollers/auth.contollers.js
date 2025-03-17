e1e325500bf805232664d43993d5aec8a3ccf8f0
rumman321 <cset-210652@dti.ac>
2025-03-17 21:54:19 +0600

    create model using mongose

--- Backend/src/contollers/auth.contollers.js
+++ Backend/src/contollers/auth.contollers.js
@@ -1,5 +1,20 @@
-export const signup = (req,res)=>{
-    res.send('signup route')
+
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+export const signup = async(req,res)=>{
+    const {email,fullName,password} = req.body;
+    try {
+        //hash password
+        if(password.length<6){
+            return res.status(400).json({message: "Password must be least 6 characters"})
+        }
+        const user = await User.findOne({email})
+        if(user) return res.status(400).json({message:"User already exists"})
+        const salt = await bcrypt.getSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+    } catch (error) {
+        
+    }
 }
 
 export const login = (req,res)=>{
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,6 +7,8 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 app.listen(PORT,()=>{
     console.log('server is running on port ',+PORT);
