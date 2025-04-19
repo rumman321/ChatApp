8a6b05a98aed862df6aa5b20047f233f8ef1b3b7
rumman321 <cset-210652@dti.ac>
2025-04-20 02:37:30 +0600

    genarate jwt token and sending it to user in a cookie

--- Backend/src/contollers/auth.contollers.js
+++ Backend/src/contollers/auth.contollers.js
@@ -1,9 +1,14 @@
 
+import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+
 export const signup = async(req,res)=>{
     const {email,fullName,password} = req.body;
     try {
+    if(!email || !fullName || !password){
+        return res.status(400).json({message:"please fill all fields"})
+    }
         //hash password
         if(password.length<6){
             return res.status(400).json({message: "Password must be least 6 characters"})
@@ -12,8 +17,29 @@ export const signup = async(req,res)=>{
         if(user) return res.status(400).json({message:"User already exists"})
         const salt = await bcrypt.getSalt(10)
         const hashedPassword = await bcrypt.hash(password,salt)
-    } catch (error) {
         
+        const newUser= new User({
+            fullName,
+            email,
+            password: hashedPassword,
+        })
+        if(newUser){
+            generateToken(newUser._id,res)
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+
+            })
+        }else{
+            res.status(400).json({message:"Invalid user data"})
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message:"Internal server error"})
     }
 }
 
